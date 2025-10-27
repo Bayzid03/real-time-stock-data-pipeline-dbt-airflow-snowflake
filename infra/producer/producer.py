75b9eda6687a0fd6e08f9d7143b1a5910620280a
Bayzid03 <143407674+Bayzid03@users.noreply.github.com>
2025-10-27 09:10:39 +0600

    Add Kafka producer for stock quotes fetching
    Implement a Kafka producer to fetch and send stock quotes.

---
+++ infra/producer/producer.py
@@ -0,0 +1,37 @@
+import time
+import json
+import requests
+from kafka import KafkaProducer
+
+API_KEY="<<YOUR API KEY>>"
+BASE_URL = "https://finnhub.io/api/v1/quote"
+SYMBOLS = ["AAPL", "MSFT", "TSLA", "GOOGL", "AMZN"]
+
+#Initial Producer
+producer = KafkaProducer (
+    bootstrap_servers=["host.docker.internal:29092"],
+    value_serializer=lambda v: json.dumps(v).encode("utf-8") # COnvert byte data to json
+)
+
+#Retrive Data
+def fetch_quote(symbol):
+    url = f"{BASE_URL}?symbol={symbol}&token={API_KEY}"
+    try:
+        response = requests.get(url)
+        response.raise_for_status()
+        data = response.json()
+        data["symbol"] = symbol
+        data["fetched_at"] = int (time.time()) # Timestamp of when the data was fetched
+        return data
+    except Exception as e:
+        print(f"Error fetching {symbol}: {e}")
+        return None
+
+#Looping and Pushing to Stream
+while True:
+    for symbol in SYMBOLS:
+        quote = fetch_quote(symbol)
+        if quote:
+            print(f"Producing: {quote}")
+            producer.send("stock-quotes", value=quote)
+    time.sleep(6)
